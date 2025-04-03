d03b78f6b569008b600b8762b0685e888ce0258a
lazycattylqm <leeqiming@163.com>
2025-04-04 00:15:05 +0800

    Add demo command and example JSON file; update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # helloworld README
 
-This is the README for your extension "helloworld". After writing up a brief description, we recommend including the following sections.
+ After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
@@ -8,7 +8,6 @@ Describe specific features of your extension including screenshots of your exten
 
 For example if there is an image subfolder under your extension project workspace:
 
-\!\[feature X\]\(images/feature-x.png\)
 
 > Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
 
--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,32 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from HelloWorld!');
 	});
 
+	const demoCommand = vscode.commands.registerCommand('helloworld.demoCommand', () => {
+		console.log('Demo command executed!');
+
+		// copy examples demo.json to user worksapce
+
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (workspaceFolders) {
+			const workspaceFolder = workspaceFolders[0];
+			const demoJsonPath = vscode.Uri.joinPath(workspaceFolder.uri, 'demo.json');
+			const demoJsonContent = JSON.stringify({ message: 'Hello World! Demo generated' }, null, 2);
+			vscode.workspace.fs.writeFile(demoJsonPath, Buffer.from(demoJsonContent));
+		}
+		// what if workspaceFolders is undefined?, to create a folder named test under the workspace folder
+		else {
+			vscode.window.showErrorMessage('No workspace folder found. Please open a workspace folder.');
+
+		}
+
+
+
+	});
+
+	context.subscriptions.push(demoCommand);
+
 	context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
